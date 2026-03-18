6e96f42db4edb59e63445c30140535574491b877
lWonJunl <tvwonjun0307@gmail.com>
2026-03-18 14:04:42 +0900

    test

---
+++ 1-1/Python_Programming/Week2/2주차 수업 실습.py
@@ -0,0 +1,26 @@
+#서식을 이용한 출력
+print("%d / %d = %5.1f" % (100, 200, 0.5))
+print("%d" % 123)
+print("%5d" % 123)
+print("%05d" % 123)
+
+#format() 함수를 이용한 출력
+print("{2:d} {1:d} {0:d}".format(100,200,300))
+
+#이스케이프 문자
+print("줄 바꿈 \n연습")
+print("탭키 \t연습")
+print("글자가 \"강조\"되는 효과1")
+print("글자가 *강조*되는 효과2")
+print("\\\\\\ 역슬래시 세 게 출력")
+print("\\n \\t \" \\ 를 그대로 출력")
+
+#코드 정지
+#import os
+# os.system("Pause")
+
+#변수의 사용
+myVer = 100
+type(myVer)
+myVer = 100.0
+type(myVer)
---
+++ 1-1/Python_Programming/Week2/self 03-02.py
@@ -0,0 +1,18 @@
+sel = int(input("입력 진수 결정(16/10/8/2) : "))
+num = input("값 입력 : ")
+
+if sel == 16:
+    num10 = int(num, 16)
+elif sel == 10:
+    num10 = int(num, 10)
+elif sel == 8:
+    num10 = int(num, 8)
+elif sel == 2:
+    num10 = int(num, 2)
+else :
+    print("16, 10, 8, 2 숫자중 하나만 입력하세요.")
+
+print("16진수 ==>", hex(num10))
+print("10진수 ==>", num10)
+print("8진수 ==>", oct(num10))
+print("2진수 ==>", bin(num10))
